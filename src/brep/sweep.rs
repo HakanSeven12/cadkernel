b959b46143fc6bb5de19c7808a909790b114ff19
ramox81 <184937705+ramox81@users.noreply.github.com>
2026-09-01 12:26:00 +0300

    Allow trimmed torus profiles in revolutions

--- src/brep/sweep.rs
+++ src/brep/sweep.rs
@@ -857,7 +857,7 @@ impl Turn {
                         }),
                         false,
                     ))
-                } else if arc.radius < centre.radius {
+                } else {
                     Some((
                         Surface::Torus(Torus {
                             frame: self.circle(centre.height)?,
@@ -866,9 +866,6 @@ impl Turn {
                         }),
                         false,
                     ))
-                } else {
-                    // The tube would reach past the axis and cross itself.
-                    None
                 }
             }
             Curve2::Ellipse(_) | Curve2::Nurbs(_) => {
